a49acddb748de343f30d2eb32d5ad79905325f49
Leonardo Lazzarin <lazzarinleonardo00@gmail.com>
2022-12-22 17:30:04 +0100

    Ongoing dev continue beer info

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,35 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationStart, Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  randomBeerVisible: boolean;
+
+  constructor(
+    private route: Router
+  ) {
+    this.randomBeerVisible = true;
+  }
+
+  ngOnInit() {
+    // Event when route change
+    this.route.events.subscribe(this.onRouterEvent);
+  }
+
+  onRouterEvent(event: any) {
+    // Check if the event is a navigation star
+    if (event instanceof NavigationStart) {
+      const navigation = event as NavigationStart;
+
+      // If the route is random beer hide the button on NavBar
+      this.randomBeerVisible = !(navigation.url === '/beer/random');
+      console.log(navigation.url);
+      console.log(this.randomBeerVisible);
+    }
+  }
 }
